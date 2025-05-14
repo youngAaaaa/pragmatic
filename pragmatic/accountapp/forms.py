0810531e8c7caf5b1719ea150f935bd5d11d5b2c
youngAaaaa <97266875+youngAaaaa@users.noreply.github.com>
2025-05-15 07:14:28 +0900

    django course 27 commit

--- pragmatic/accountapp/forms.py
+++ pragmatic/accountapp/forms.py
@@ -1,8 +1,22 @@
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.models import User
 
 class AccountUpdateForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.fields['username'].disabled = True
+
+class AccountUpdateForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # UserCreationForm 필드 중 username 필드는 수정 못 하도록 잠금
+        self.fields["username"].disabled = True
+
+    def clean_username(self):
+        # 현재 사용자명은 이미 존재하는 것으로 간주
+        return self.instance.username
--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -14,18 +14,22 @@ from accountapp.models import HelloWorld
 
 def hello_world(request):
 
-    if request.method == "POST":
-        temp = request.POST.get('hello_world_input')
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            temp = request.POST.get('hello_world_input')
 
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
+            new_hello_world = HelloWorld()
+            new_hello_world.text = temp
+            new_hello_world.save()
 
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+            return HttpResponseRedirect(reverse('accountapp:hello_world'))
+
+        else:
+            hello_world_list = HelloWorld.objects.all()
+            return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
     else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+        return HttpResponseRedirect(reverse('accountapp:login'))
 
 class AccountCreateView(CreateView):
     model = User
@@ -44,6 +48,8 @@ class AccountUpdateView(UpdateView):
     form_class = AccountUpdateForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+    def get_success_url(self):
+        return reverse("accountapp:detail", kwargs={'pk': self.object.pk})
 
 class AccountDeleteView(DeleteView):
     model = User
