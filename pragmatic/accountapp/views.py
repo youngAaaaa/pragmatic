458d7ef9a5d19de390db761ff7d4ffacedea9934
youngAaaaa <97266875+youngAaaaa@users.noreply.github.com>
2025-06-16 07:04:11 +0900

    django course 28 commit

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -1,19 +1,22 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
 
-# Create your views here.
+has_ownership = [account_ownership_required, login_required]
 
 
+@login_required
 def hello_world(request):
-
     if request.user.is_authenticated:
         if request.method == "POST":
             temp = request.POST.get('hello_world_input')
@@ -28,9 +31,6 @@ def hello_world(request):
             hello_world_list = HelloWorld.objects.all()
             return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
-    else:
-        return HttpResponseRedirect(reverse('accountapp:login'))
-
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
@@ -42,15 +42,17 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
     form_class = AccountUpdateForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
-    def get_success_url(self):
-        return reverse("accountapp:detail", kwargs={'pk': self.object.pk})
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
